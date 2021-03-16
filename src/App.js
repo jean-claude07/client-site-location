57f19eb57c9e24c22e2453380a1bc1de43e0fd77
jean-claude07 <jeclaude78@gmail.com>
2021-03-16 11:11:21 +0300

    netly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch, HashRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css'
 import Footer from './components/Footer';
 import Header from './components/Header';
